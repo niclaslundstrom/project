e5d4253ab6cc204c7401a57c5a67af865f185948
NiclasLundstrom <lille_niclas@hotmail.com>
2021-02-01 09:25:26 +0100

    sista comit innan inläming!

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,24 @@ module.exports = {
       background_color: '#003956'
     },
     name: 'Yes or no!',
-    themeColor: 'red'
+    themeColor: '#509bf5',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern: 'https://avancera.app/cities/'
+        },
+        {
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern: 'https://cors-anywhere.herokuapp.com/http://yesno.wtf/api/'
+        }
+      ]
+    }
   }
 }
